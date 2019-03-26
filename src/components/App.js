aabfcb86b00528b431ab2180124e48421b1adea4
russellong95 <digivicer@hotmail.com>
2019-03-26 14:29:02 +0800

    added pages for both charts

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Header from "./Header";
 import { BrowserRouter, Route } from "react-router-dom";
 import Homepage from "../pages/Homepage"
 import Savingspage from "../pages/Savingspage";
+import HomepageCD from './../pages/Homepage.CD';
+import HomepageSpending from './../pages/Homepage.Spending';
 
 export default class App extends React.Component {
   render() {
@@ -12,6 +14,8 @@ export default class App extends React.Component {
         <div>
           <Route path="/" exact component={Homepage} />
           <Route path="/savings" component={Savingspage} />
+          <Route path="/cd" component={HomepageCD} />
+          <Route path="/spending" component={HomepageSpending} />
         </div>
       </BrowserRouter>
     );
---
+++ src/components/Stackchart.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Barchart.css"
+import jsonFile from '../data/personal_data.json'
+
+function BarGroup(props) {
+  let barPadding = 2
+  let barColour = '#348AA7'
+  let widthScale = d => d * 10
+
+  let width = widthScale(props.d.value)
+  let yMid = props.barHeight * 0.5
+  
+  return <g className="bar-group">
+    <text className="name-label" x="-6" y={yMid} alignmentBaseline="middle" >{props.d.name}</text>
+    <rect y={barPadding * 0.5} width={width} height={props.barHeight - barPadding} fill={barColour} />
+    <text className="value-label" x={width- 8} y={yMid} alignmentBaseline="middle" >{props.d.value}</text>
+  </g>
+}
+
+class Barchart extends React.Component {
+  state = {
+    data: [
+      { name: 'Credit', value: 100 },
+      { name: 'Debit', value: 80 },
+    ]
+  }
+
+  render() {
+    let barHeight = 30
+    let barGroups = this.state.data.map((d, i) => <g transform={`translate(0, ${i * barHeight})`}>
+                                                    <BarGroup d={d} barHeight={barHeight} />
+                                                  </g>)                         
+    
+    return <svg width="800" height="300" >
+      <g className="container">
+        <text className="title" x="10" y="30">Did you know? Credit payments are worse than debit payments!</text>
+        <g className="chart" transform="translate(100,60)">
+          {barGroups}
+        </g>
+      </g>
+    </svg>
+  }
+}
+
+export default Barchart;
