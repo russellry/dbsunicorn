83d0de0898c0184c0c4afa0a989053b38d8edf74
zhx <zhanghx96@gmail.com>
2019-03-26 13:59:18 +0800

    Updated sorted transactions

--- src/data/sort_transactions.py
+++ src/data/sort_transactions.py
@@ -25,11 +25,11 @@ def sort_transaction(data):
     for t in data:
         datestr = t["date"]
         tag = t['tag']
-        amount = float(t["amount"])
+        amount = round(float(t["amount"]), 2)
 
         dateobj = datetime.strptime(datestr, "%Y-%m-%dT%H:%M:%S.%f%z")
         part_of_day = get_part_of_day(dateobj)
-        
+
         # tag
         if tag in res[part_of_day].keys():
             res[part_of_day][tag] += amount
@@ -37,13 +37,32 @@ def sort_transaction(data):
             res[part_of_day][tag] = amount
 
     return res
-        
+
+
+def sort_transaction_by_cat(data):
+    """ list of transaction in dict, return dict of sorted transations """
+
+    res = dict()
+    for t in data:
+        tag = t['tag']
+        amount = round(float(t["amount"]), 2)
+
+        # tag
+        if tag in res.keys():
+            res[tag] += amount
+        else:
+            res[tag] = amount
+
+    return res
+
 
 def test():
     data = read_json('personal_data.json')
     transaction_sample = data["2"]["debitAccounts"]["74"]["transactions"]
     res = sort_transaction(transaction_sample)
     write_json("marytan_past_transactions_sorted.json", res)
+    res2 = sort_transaction_by_cat(transaction_sample)
+    write_json("mary_tan_transactions_by_tag.json", res2)
 
 if __name__ == '__main__':
     test()
